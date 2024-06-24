123cc3779baed15b254efbebf8d7c1c09f6938d0
Aarón J. Montes <aaronmontes_92@yahoo.es>
2024-06-24 15:08:22 -0600

    feat: 🎸 Initial configuration for twind/core

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
+import { install } from '@twind/core'
+import config from '../twind.config'
+
+install(config)
 
 createApp(App)
   .use(router)
